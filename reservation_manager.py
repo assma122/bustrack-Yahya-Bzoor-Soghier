08d524708f3526978815a2c3326fcbcb2b0cabba
assma122 <asmayahya330@gmail.com>
2026-06-02 20:45:12 +0300

    Add reservation manager structure and load method

---
+++ reservation_manager.py
@@ -0,0 +1,20 @@
+import csv 
+class ReservationManager: 
+  def load_reservations(self,filepath:str)-> list:
+    try:
+        with open(filepath,"r") as file:
+           return list(csv.DictReader(file))
+    except FileNotFoundError:
+       return []    
+     
+  def save_reservations(self,reservations:list,filepath:str)->None :
+     pass  
+
+  def cancel(self,reservations:list,res_id:str)->list:
+    pass 
+  def get_by_route(self,reservations:list,route_id:str)->list:
+    pass      
+  def book(self,reservations:list,routes:list 
+           ,student_name:str,student_id:str,route_id:str,date:str,bus_manager)->list:
+       pass 
+  
